012ea978b64a55f701bb8fb857217bd8a48af88c
whatmurder <jonasrinne@proton.me>
2024-04-22 16:52:48 +0300

    Optimizations

--- k24TiimiNROFrontEnd/src/components/ProductList.jsx
+++ k24TiimiNROFrontEnd/src/components/ProductList.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { AgGridReact } from "ag-grid-react";
+import {useEffect, useState} from 'react';
+import {AgGridReact} from "ag-grid-react";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
@@ -11,23 +11,23 @@ export default function ProductList() {
 
     const fetchData = () => {
         fetch('http://localhost:8080/api/clothes')
-        .then(response => response.json())
-        .then(data => setProducts(data))
-        .catch(err => console.error(err))
+            .then(response => response.json())
+            .then(data => setProducts(data))
+            .catch(err => console.error(err))
     }
 
-    const [columnDefs, setColumnDefs] = useState([
-        {field: 'title', sortable: true, filter: true },
-        {field: 'category', sortable: true, filter: true },
-        {field: 'color', sortable: true, filter: true },
-        {field: 'size', sortable: true, filter: true },
-        {field: 'price', sortable: true, filter: true },
-        {field: 'manufacturer', sortable: true, filter: true },
+    const [columnDefs] = useState([
+        {field: 'title', sortable: true, filter: true},
+        {field: 'category', sortable: true, filter: true},
+        {field: 'color', sortable: true, filter: true},
+        {field: 'size', sortable: true, filter: true},
+        {field: 'price', sortable: true, filter: true},
+        {field: 'manufacturer', sortable: true, filter: true},
     ]);
 
     return (
         <>
-            <div className="ag-theme-material" style={{ width: 1280, height: 1000}}>
+            <div className="ag-theme-material" style={{width: 1280, height: 1000}}>
                 <AgGridReact
                     rowData={products}
                     columnDefs={columnDefs}
