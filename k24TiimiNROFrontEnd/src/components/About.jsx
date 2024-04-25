6223d98ac5ee859ed251646237c6fd44ee97f6c0
Ponki Phan <ponkiphan@gmail.com>
2024-04-25 13:18:26 +0300

    m

--- k24TiimiNROFrontEnd/src/components/About.jsx
+++ k24TiimiNROFrontEnd/src/components/About.jsx
@@ -1,15 +1,23 @@
 export default function About() {
-    return <>
-        <h1>About</h1>
-        <h4>
-            Dog Store was created to deliver high-quality, functional, comfortable pet essentials at accessible prices.
-        </h4>
-        <h5>
-            Hello fellow dog parents! We're not the typical pet industry veterans, our journey with Dog Store began in 2017 with a simple problem: finding quality yet affordable clothes for our Frenchie Stella was mission impossible. That was our 'aha!' moment- there is a serious need for quality dog gear that doesn't cost an arm and a leg!
-            Our approach is straightforward: quality is the most important. We use superior materials, custom made-for-dog fabrics, and pay attention to the little details that matter.
-            The "FIT" is just as important! We've measured thousands of pooches, creating an extensive database of doggie dimensions to ensure our gear fits every pup like a glove.
-            Our goal? To bring your dog top-quality products and spark joy between pets and people.
-            Thanks for joining us on this journey. Here's to wagging tails!
-        </h5>
-    </>
+    return (
+        <>
+            <h1>About</h1>
+            <img src="path_to_your_image.jpg" alt="Frenchie Photo" style={{ width: '200px', height: '200px' }} />
+            <p>
+                Dog Store was created to deliver high-quality, functional, comfortable pet essentials at accessible prices.
+                <br />
+                <br />
+                Hello fellow dog parents! We're not the typical pet industry veterans, our journey with Dog Store began in 2017 with a simple problem: finding quality yet affordable clothes for our Frenchie Stella was mission impossible. That was our 'aha!' moment- there is a serious need for quality dog gear that doesn't cost an arm and a leg!
+                Our approach is straightforward: quality is the most important. We use superior materials, custom made-for-dog fabrics, and pay attention to the little details that matter.
+                The "FIT" is just as important! We've measured thousands of pooches, creating an extensive database of doggie dimensions to ensure our gear fits every pup like a glove.
+                Our goal? To bring your dog top-quality products and spark joy between pets and people.
+                Thanks for joining us on this journey. Here's to wagging tails!
+            </p>
+            <p>
+                Sincerely Jonas, Duy, Eki, Kasper & Ponki
+                <br />
+                Business Identity Code: 1234567-8
+            </p>
+        </>
+    );
 }
