d7d90243993d2a2e024066ad6f49b7febbf359b3
whatmurder <jonasrinne@proton.me>
2024-04-29 16:28:30 +0300

    Register v2

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,71 +1,56 @@
-import {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {Button, Stack, TextField} from "@mui/material";
 
 export default function Register() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
 
     useEffect(() => fetchUsers(), []);
 
+    const [user, setUser] = React.useState({username: "", password: "", role: "USER"});
 
-    // Temporary function to fetch users from the API
     const fetchUsers = () => {
         fetch('http://localhost:8080/api/appusers')
             .then(response => response.json())
             .then(data => console.log(data))
             .catch(err => console.error(err))
+    };
+
+    const handleInputChange = (event) => {
+        setUser({...user, [event.target.name]: event.target.value});
+    };
+
+    const registerUser = (user) => {
+        fetch('http://localhost:8080/api/appusers', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err))
     }
 
-
-    // ToDo: Figure out the COSR issue ASAP!
-    const registerUser = async () => {
-        try {
-            const response = await fetch('http://localhost:8080/api/appusers', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({username, password, role: 'USER'}), // Always set role to 'USER'
-            });
-
-            if (!response.ok) {
-                const data = await response.json();
-                console.error('Error:', data);
-                setError('Error registering user!');
-            } else {
-                console.log('User registered');
-            }
-        } catch (error) {
-            console.error('Network error:', error);
-        }
-    };
-
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
-    };
-
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
-
     return (<>
-        <h2>Login or Register:</h2>
-        {error && <p>{error}</p>}
+        <h2>Register:</h2>
         <Stack direction="column" spacing={2} justifyContent="center" alignItems="center">
             <TextField
+                name="username"
+                value={user.username}
                 autoFocus
                 label="Username"
-                onChange={handleUsernameChange}
+                onChange={handleInputChange}
             />
             <TextField
+                name="password"
+                value={user.password}
                 label="Password"
                 type="password"
-                onChange={handlePasswordChange}
+                onChange={handleInputChange}
             />
         </Stack>
         <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
-            <Button onClick={registerUser}>Register</Button>
+            <Button onClick={() => registerUser(user)}>Register</Button>
         </Stack>
     </>);
 }
