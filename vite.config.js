8024159f92d57f35c87d06a5abbfe4931396390b
whatmurder <jonasrinne@proton.me>
2024-05-09 16:59:07 +0300

    base changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/k24Tiimi3FrontEnd/',
+    base: '/k24tiimi3frontend/',
     plugins: [react()],
     test: {
         globals: true,
